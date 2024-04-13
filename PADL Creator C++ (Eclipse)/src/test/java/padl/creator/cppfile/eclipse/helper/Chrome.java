907729a7dd02c3a1b6e50dc5ba8802b6c160f858
Omer Munam <39843323+omer-munam@users.noreply.github.com>
2024-04-13 01:27:57 -0400

    Revert "Working on PADL Creator C++ (Eclipse)"
    This reverts commit 58c7ba65ea317e640f5c5c200763b27f3a5a4588.

--- PADL Creator C++ (Eclipse)/src/test/java/padl/creator/cppfile/eclipse/helper/Chrome.java
+++ PADL Creator C++ (Eclipse)/src/test/java/padl/creator/cppfile/eclipse/helper/Chrome.java
@@ -10,31 +10,26 @@
  ******************************************************************************/
 package padl.creator.cppfile.eclipse.helper;
 
-import padl.cpp.kernel.impl.CPPFactoryEclipse;
-import padl.kernel.ICodeLevelModel;
-import padl.kernel.ICodeLevelModelCreator;
-import padl.kernel.exception.CreationException;
+import padl.generator.helper.ModelGenerator;
+import padl.kernel.IIdiomLevelModel;
 import padl.util.ModelStatistics;
 
 public class Chrome {
-	public static void main(final String[] args) throws CreationException {
+	public static void main(final String[] args) {
 		final ModelStatistics statistics = new ModelStatistics();
-
-		//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/net/",
-		//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/history/",
-		//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/sync/",
-		//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/chromeos/",
-		//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/extensions/",
-		//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/",
-		final ICodeLevelModelCreator creator = new padl.creator.cppfile.eclipse.CPPCreator(
-				"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/ui/");
-		final ICodeLevelModel codeLevelModel = CPPFactoryEclipse.getInstance()
-				.createCodeLevelModel(
-						"Various chromium/src/chrome/browser/ subfolders");
-		codeLevelModel.addModelListener(statistics);
-		codeLevelModel.create(creator);
-
-		System.out.println(codeLevelModel);
+		final IIdiomLevelModel idiomLevelModel =
+			ModelGenerator
+				.generateModelFromCppFilesUsingEclipse(
+					"Various chromium/src/chrome/browser/ subfolders",
+					//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/net/",
+					//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/history/",
+					//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/sync/",
+					//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/chromeos/",
+					//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/extensions/",
+					"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/ui/",
+					//	"C:/Temp/Chromium/chromium.r197479/home/src_tarball/tarball/chromium/src/chrome/browser/",
+					statistics);
+		System.out.println(idiomLevelModel);
 		System.out.println(statistics);
 	}
 }
